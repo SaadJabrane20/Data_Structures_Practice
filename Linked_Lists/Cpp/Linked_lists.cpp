8803b80cdd3dd667214563643f2c9f0aa06eb325
SaadJabrane20 <sjabrane20@gmail.com>
2025-09-14 23:42:25 +0100

    Linked lists in cpp

--- Linked_Lists/Cpp/Linked_lists.cpp
+++ Linked_Lists/Cpp/Linked_lists.cpp
@@ -18,26 +18,30 @@ class Linked_List{
             head = nullptr;
         }
 
-        //insert in the beginning of the list
+        //1. insert in the beginning of the list
         Linked_List insert_beginning(int v){
             Node *new_node = new Node(v);
             new_node->next = head;
             head = new_node;
+            return *this;
         }
 
-        //insert in the end of the list
+        //2. insert in the end of the list
         Linked_List insert_end(int v)
         {
             Node *current = head;
             Node *new_node = new Node(v);
-            if(head == nullptr)
+            if(head == nullptr){
                 insert_beginning(v);
+                return *this;
+            }
+
             while(current->next != nullptr)
                 current = current->next;
             current->next = new_node;
         }
 
-        //insert in position
+        //3. insert in position
         Linked_List insert_position(int v, int position)
         {
             Node *current = head;
@@ -64,17 +68,100 @@ class Linked_List{
             return *this;
         }
 
-        //Display list elements
-        void display(){
+        //4. delete last node
+        Linked_List delete_last()
+        {
             if(head == nullptr)
-                cout<<"List is empty!\n";
-            while(head != nullptr)
+            {    
+                cout<<"your list is empty!\n";
+                return *this;
+            }
+
+            if(head->next == nullptr)
             {
-                cout<<head->value<<" -> ";
-                head = head->next;
+                delete(head);
+                head = nullptr;
+                return *this;
+            }
+            
+            Node *current = head;
+            while(current->next->next != nullptr){
+                current = current->next;
+            }
+            Node *temp = current->next;
+            delete(temp);
+            current->next = nullptr;
+            return *this;
+        }
+        
+        //5. delete beginning
+        Linked_List delete_beginning(){
+            if(head == nullptr){
+                cout<<"Your list is empty!\n";
+                return *this;
+            }
+            Node *temp = head;
+            head = head->next;
+            delete temp;
+            return *this;
+        }
+
+        //6. delete position
+        Linked_List delete_position(int pos){
+            if(head == nullptr){
+                cout<<"your list is empty\n";
+                return *this;
+            }
+            if(pos == 0){
+                delete_beginning();
+                return *this;
+            }
+            Node *current = head;
+            int counter = 0;
+            while(current != nullptr && counter < pos - 1){
+                current = current->next;
+                counter++;
+            }
+            if(current == nullptr || current->next == nullptr){
+                cout<<"position out of range !\n";
+                return *this;
+            }
+            Node *temp = current->next;
+            current->next = current->next->next;
+            delete temp;
+            return *this;
+        }
+
+
+        //7. Display list elements
+        void display(){
+            Node *current = head;
+            if(current == nullptr)
+                cout<<"List is empty!\n";
+            while(current != nullptr)
+            {
+                cout<<current->value<<" -> ";
+                current = current->next;
             }
             cout << "NULL\n";
         }
+
+        //8. search element in the list
+
+        
+
+
+
+        // ~Linked_List(){
+        //     Node* current = head;
+        //     while(current != nullptr){
+        //         Node* temp = current;
+        //         current = current->next;
+        //         delete temp;
+        //     }
+        //     head = nullptr;
+        //     cout << "Your list was destroyed !\n";
+        // };
 };
 
 int main(){
@@ -84,5 +171,7 @@ int main(){
     list.insert_beginning(30);
     list.insert_end(69);
     list.insert_position(100,0);
+    list.display();
+    list.delete_position(1);
     list.display();
 }
